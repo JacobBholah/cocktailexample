843d89ba73e04dd47ab0160593c073e92ce80375
Jacob Bholah <jbholah@softwareinstitute.com>
2021-12-10 12:26:15 +0000

    removed tests

--- src/test/java/com/cocktailexample/cocktailexample/CocktailexampleApplicationTests.java
+++ src/test/java/com/cocktailexample/cocktailexample/CocktailexampleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cocktailexample.cocktailexample;
+/*package com.cocktailexample.cocktailexample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class CocktailexampleApplicationTests {
 	}
 
 }
+*/
