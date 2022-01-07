ca7dada443dbac5dd2d2e761f227ba47d2de5091
Jacob Bholah <jbholah@softwareinstitute.com>
2022-01-07 17:08:37 +0000

    more mokito tests

--- src/test/java/com/cocktailexample/cocktailexampletests/MockitoTest.java
+++ src/test/java/com/cocktailexample/cocktailexampletests/MockitoTest.java
@@ -43,7 +43,7 @@ public class MockitoTest {
         MockitoAnnotations.openMocks(this);
         cocktailexampleApplication = new CocktailexampleApplication(ingredientrepository, glassrepository, garnishrepository, equipmentrepository, instructionrepository, cocktailinstructionsrepository, cocktailrepository);
     }
-      @org.junit.Test
+      @Test
     public void testgetAllingreients()
 {
     cocktailexampleApplication.getAllingredients();
@@ -69,17 +69,213 @@ public class MockitoTest {
 
         ingredient capturedingredient = ingredientArgumentCaptor.getValue();
 
-        System.out.println(savedingredient.toString());
-        System.out.println(capturedingredient);
-
         assertEquals("wrong name",savedingredient.getName(),capturedingredient.getName());
-        assertEquals("wrong name",savedingredient.getType(),capturedingredient.getType());
-        assertEquals("wrong name",savedingredient.getABV(),capturedingredient.getABV());
-        assertEquals("wrong name",savedingredient.getStorage(),capturedingredient.getStorage());
-        assertEquals("wrong name",savedingredient.getDescription(),capturedingredient.getDescription());
-
+        assertEquals("wrong type",savedingredient.getType(),capturedingredient.getType());
+        assertEquals("wrong ABV",savedingredient.getABV(),capturedingredient.getABV());
+        assertEquals("wrong storage",savedingredient.getStorage(),capturedingredient.getStorage());
+        assertEquals("wrong description",savedingredient.getDescription(),capturedingredient.getDescription());
 
         assertEquals("wrong message",expected,actual);
 
     }
+
+//    @Test
+//    public void testdeleteingredient(){
+//
+//        String expected = "deleted";
+//
+//        String actual = cocktailexampleApplication.deleteingredient(ingredient.)
+//    }
+
+
+
+    @Test
+    public void testgetAllglasses()
+    {
+        cocktailexampleApplication.getAllglasses();
+        verify(glassrepository).findAll();
+    }
+
+
+    @Test
+    public void testaddglass(){
+        glass savedglass = new glass();
+        savedglass.setType("shot");
+        savedglass.setVolume(44);
+
+        String expected = "Saved";
+
+        String actual = cocktailexampleApplication.addAglass(savedglass.getType(),savedglass.getVolume());
+
+        ArgumentCaptor<glass> glassArgumentCaptor = ArgumentCaptor.forClass(glass.class);
+
+        verify(glassrepository).save(glassArgumentCaptor.capture());
+
+        glass capturedglass = glassArgumentCaptor.getValue();
+
+        assertEquals("wrong type",savedglass.getType(),capturedglass.getType());
+        assertEquals("wrong volume",savedglass.getVolume(),capturedglass.getVolume());
+
+        assertEquals("wrong message",expected,actual);
+
+    }
+
+    @Test
+    public void testgetAllgarnish()
+    {
+        cocktailexampleApplication.getAllgarnishs();
+        verify(garnishrepository).findAll();
+    }
+    @Test
+    public void testaddgarnish(){
+        garnish savedgarnish = new garnish();
+        savedgarnish.setId(1);
+        savedgarnish.setName("lemon slice");
+        savedgarnish.setType("fruit");
+
+
+
+
+        String expected = "Saved";
+
+        String actual = cocktailexampleApplication.addAgarnish(savedgarnish.getId(),savedgarnish.getName(),savedgarnish.getType());
+
+        ArgumentCaptor<garnish> garnishArgumentCaptor = ArgumentCaptor.forClass(garnish.class);
+
+        verify(garnishrepository).save(garnishArgumentCaptor.capture());
+
+        garnish capturedgarnish = garnishArgumentCaptor.getValue();
+
+        assertEquals("wrong Id",savedgarnish.getId(),capturedgarnish.getId());
+        assertEquals("wrong name",savedgarnish.getName(),capturedgarnish.getName());
+        assertEquals("wrong type",savedgarnish.getType(),capturedgarnish.getType());
+
+        assertEquals("wrong message",expected,actual);
+
+    }
+    @Test
+    public void testgetAllequipment()
+    {
+        cocktailexampleApplication.getAllequipment();
+        verify(equipmentrepository).findAll();
+    }
+    @Test
+    public void testaddequipment(){
+        equipment savedequipment = new equipment();
+        savedequipment.setName("shaker");
+        savedequipment.setType("hand");
+
+
+
+
+        String expected = "Saved";
+
+        String actual = cocktailexampleApplication.addequipment(savedequipment.getName(),savedequipment.getType());
+
+        ArgumentCaptor<equipment> equipmentArgumentCaptor = ArgumentCaptor.forClass(equipment.class);
+
+        verify(equipmentrepository).save(equipmentArgumentCaptor.capture());
+
+        equipment capturedequipment = equipmentArgumentCaptor.getValue();
+
+        assertEquals("wrong name",savedequipment.getName(),capturedequipment.getName());
+        assertEquals("wrong type",savedequipment.getType(),capturedequipment.getType());
+
+        assertEquals("wrong message",expected,actual);
+
+    }
+    @Test
+    public void testgetAllinstructions()
+    {
+        cocktailexampleApplication.getAllinstructions();
+        verify(instructionrepository).findAll();
+    }
+    @Test
+    public void testaddinstruction(){
+        instruction savedinstruction = new instruction();
+        savedinstruction.setInstructionid(1);
+        savedinstruction.setDescription("description");
+        savedinstruction.setGlass_Type("shot");
+        savedinstruction.setGarnish_Name("lemon slice");
+
+        String expected = "Saved";
+
+        String actual = cocktailexampleApplication.addinstruction(savedinstruction.getInstructionid(),savedinstruction.getDescription(),savedinstruction.getGlass_Type(),savedinstruction.getGarnish_Name());
+
+        ArgumentCaptor<instruction> instructionArgumentCaptor = ArgumentCaptor.forClass(instruction.class);
+
+        verify(instructionrepository).save(instructionArgumentCaptor.capture());
+
+        instruction capturedinstruction = instructionArgumentCaptor.getValue();
+
+        assertEquals("wrong name",savedinstruction.getInstructionid(),capturedinstruction.getInstructionid());
+        assertEquals("wrong type",savedinstruction.getDescription(),capturedinstruction.getDescription());
+        assertEquals("wrong glass type",savedinstruction.getGlass_Type(),capturedinstruction.getGlass_Type());
+        assertEquals("wrong garnish name",savedinstruction.getGarnish_Name(),capturedinstruction.getGarnish_Name());
+
+        assertEquals("wrong message",expected,actual);
+
+    }
+
+    @Test
+    public void testgetAllcocktailinstructions()
+    {
+        cocktailexampleApplication.getAllcocktailinstructions();
+        verify(cocktailinstructionsrepository).findAll();
+    }
+    @Test
+    public void testaddcocktailinstruction(){
+        cocktailinstructions savedcocktailinstructions = new cocktailinstructions();
+        savedcocktailinstructions.setCocktailinstructionsid(1);
+        savedcocktailinstructions.setStepnumber(2);
+        savedcocktailinstructions.setInstructionid(3);
+        savedcocktailinstructions.setCocktail_Name("Long Island");
+
+        String expected = "Saved";
+
+        String actual = cocktailexampleApplication.addcocktailinstruction(savedcocktailinstructions.getCocktailinstructionsid(),savedcocktailinstructions.getStepnumber(),savedcocktailinstructions.getInstructionid(),savedcocktailinstructions.getCocktail_Name());
+
+        ArgumentCaptor<cocktailinstructions> cocktailinstructionsArgumentCaptor = ArgumentCaptor.forClass(cocktailinstructions.class);
+
+        verify(cocktailinstructionsrepository).save(cocktailinstructionsArgumentCaptor.capture());
+
+        cocktailinstructions capturedcocktailinstructions = cocktailinstructionsArgumentCaptor.getValue();
+
+        assertEquals("wrong cocktail instruction id",savedcocktailinstructions.getCocktailinstructionsid(),capturedcocktailinstructions.getCocktailinstructionsid());
+        assertEquals("wrong stepnumbber",savedcocktailinstructions.getStepnumber(),capturedcocktailinstructions.getStepnumber());
+        assertEquals("wrong instruction id",savedcocktailinstructions.getInstructionid(),capturedcocktailinstructions.getInstructionid());
+        assertEquals("wrong cocktail name",savedcocktailinstructions.getCocktail_Name(),capturedcocktailinstructions.getCocktail_Name());
+
+        assertEquals("wrong message",expected,actual);
+
+    }
+    @Test
+    public void testgetAllcocktails()
+    {
+        cocktailexampleApplication.getAllcocktails();
+        verify(cocktailrepository).findAll();
+    }
+    @Test
+    public void testaddcocktail(){
+        cocktail savedcocktail = new cocktail();
+        savedcocktail.setName("Long Island");
+        savedcocktail.setType("Alcoholic");
+
+        String expected = "Saved";
+
+        String actual = cocktailexampleApplication.addcocktail(savedcocktail.getName(),savedcocktail.getType());
+
+        ArgumentCaptor<cocktail> cocktailArgumentCaptor = ArgumentCaptor.forClass(cocktail.class);
+
+        verify(cocktailrepository).save(cocktailArgumentCaptor.capture());
+
+        cocktail capturedcocktail = cocktailArgumentCaptor.getValue();
+
+        assertEquals("wrong name",savedcocktail.getName(),capturedcocktail.getName());
+        assertEquals("wrong type",savedcocktail.getType(),capturedcocktail.getType());
+
+        assertEquals("wrong message",expected,actual);
+
+    }
+
 }
